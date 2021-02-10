bd4f0341900adaff18f356a4b43def4eac4e320d
krjw-00 <64105912+krjw-00@users.noreply.github.com>
2021-02-10 13:32:55 +1300

    Main script

---
+++ pinhack.py
@@ -0,0 +1,24 @@
+# Start
+def start_test():
+    import time
+    start = time.time()
+    return start
+
+# End
+def end_test(start):
+    import time
+    end = time.time()
+
+    # Summary
+    print(f"Start: {time.ctime(start)}")
+    print(f"End: {time.ctime(end)}")
+
+    execution = end - start
+    rounded = round(execution, 4)
+    print(f"Execution time: {rounded} seconds")
+
+# Operation
+start = start_test()
+range_num = 10000
+[print(i) for i in range(range_num)]
+end_test(start)
